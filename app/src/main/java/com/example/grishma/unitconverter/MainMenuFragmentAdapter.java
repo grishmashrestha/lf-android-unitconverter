b252a51f7bbc4a90c0bbe2c52149c0d20245db81
Grishma Shrestha <grishmashrestha@lftechnology.com>
2016-02-23 15:14:40 +0545

    create MainMenuFragmentAdapter

---
+++ app/src/main/java/com/example/grishma/unitconverter/MainMenuFragmentAdapter.java
@@ -0,0 +1,24 @@
+package com.example.grishma.unitconverter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by grishma on 2/23/16.
+ */
+public class MainMenuFragmentAdapter extends FragmentPagerAdapter {
+    public MainMenuFragmentAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
